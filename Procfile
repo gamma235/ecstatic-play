67ee89d5163abbaa8a0c032f05b17cd171c270fb
Jesse <jesseluisdiaz@gmail.com>
2014-06-06 19:51:04 +0900

    without uberjar

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: lein with-profile production trampoline run -m ecstatic_play.web
+web: lein with-profile production trampoline run -m ecstatic-play.web
--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject ecstatic-play "1.0.0-SNAPSHOT"
   :description "the personal website of spy, artist, lover... Jesse Diaz"
-  :url "http://ecstatic-play.herokuapp.com"
+  :url "http://stark-falls-1219.herokuapp.com/"
   :license {:name "to kill"
-            :url "https://fierce-island-4087.herokuapp.com/"}
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [compojure "1.1.1"]
                  [ring/ring-jetty-adapter "1.1.0"]
@@ -10,8 +10,7 @@
                  [ring-basic-authentication "1.0.1"]
                  [environ "0.2.1"]
                  [com.cemerick/drawbridge "0.0.6"]]
-  :uberjar-name "ecstatic-play-standalone.jar"
   :min-lein-version "2.0.0"
-  :plugins [[environ/environ.lein "0.2.1"]]
+  :plugins [[environ/environ.lein "0.2.1"] [lein-ring "0.8.7"]]
   :hooks [environ.leiningen.hooks]
   :profiles {:production {:env {:production true}}})
